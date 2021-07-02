231e6ab083d11c9ccd904fd63e159e95e1794093
hegworks <hess.gha@gmail.com>
2021-07-02 16:39:55 +0430

    turned SignIn from class to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,51 +1,42 @@
-import React from "react"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-class SignIn extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			creds: "",
-			password: ""
-		};
-		this.handleChange = this.handleChange.bind(this);
-	}
+import { useHistory } from "react-router";
 
-	handleChange(event) {
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-	}
+const SignIn = props => {
+	// states
+	const [creds, setCreds] = useState("");
+	const [password, setPssword] = useState("");
 
-	render() {
-		return (
-			<div>
-				<h1>Welcome to SMA</h1>
-				<form onSubmit={this.handleSubmit}>
-					<input
-						type="text"
-						value={this.state.creds}
-						name="creds"
-						placeholder="Email or Username"
-						onChange={this.handleChange}
-					/>
-					<input
-						type="password"
-						value={this.state.password}
-						name="password"
-						placeholder="Password"
-						onChange={this.handleChange}
-					/>
-					<button>Submit</button>
-				</form>
-				<h2>
-					Don't have an account?{" "}
-					<Link to="/signup">Sign Up here</Link>
-				</h2>
-			</div>
-		);
-	}
-}
+	// functions
+	const handleSubmit = async e => {};
 
-export default SignIn
+	return (
+		<div>
+			<h1>Welcome to SMA</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={creds}
+					name="creds"
+					placeholder="Email or Username"
+					onChange={event => setCreds(event.target.value)}
+				/>
+				<input
+					type="password"
+					value={password}
+					name="password"
+					placeholder="Password"
+					onChange={event => setPssword(event.target.value)}
+				/>
+				<button>Submit</button>
+			</form>
+			<h2>
+				Don't have an account? <Link to="/signup">Sign Up here</Link>
+			</h2>
+		</div>
+	);
+};
+
+export default SignIn;
