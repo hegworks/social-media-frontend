06cb1a3a8b8c544cd1478012fab0312cee3c685b
hegworks <hess.gha@gmail.com>
2021-07-03 18:56:22 +0430

    show AddPost only in self profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,38 +1,44 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router";
+
+import { CookieManager } from "./CookieManager";
 import Topbar from "./Topbar";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
+import AddPost from "./AddPost";
+
 import "./styles/profile.scss";
-import { useParams } from "react-router";
 
-const Profile = (props) => {
-  const userid = useParams().id;
-  console.log(userid)
+const Profile = props => {
+	const urluserid = useParams().id;
+	console.log(urluserid);
 
-  return (
-    <div className="page">
-      <Topbar />
-      <div className="profile">
-        <SideBar />
-        <div className="profileRight">
-          <div className="profileRightTop">
-            <div className="profileCover">
-              <img
-                className="profileCoverImg"
-                alt=""
-              />
-            </div>
-            <div className="profileInfo">
-              <h4 className="profileInfoName">username goes here</h4>
-              <span className="profileInfoDesc">description</span>
-            </div>
-          </div>
-          <div className="profileRightBottom">
-            <Feed profileid={userid} />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+	const { userid } = useContext(CookieManager);
+	console.log(userid);
 
-export default Profile
+	return (
+		<div className="page">
+			<Topbar />
+			<div className="profile">
+				<SideBar />
+				<div className="profileRight">
+					<div className="profileRightTop">
+						<div className="profileCover">
+							<img className="profileCoverImg" alt="" />
+						</div>
+						<div className="profileInfo">
+							<h4 className="profileInfoName">username goes here</h4>
+							<span className="profileInfoDesc">description</span>
+						</div>
+					</div>
+					<div>{userid === urluserid && <AddPost />}</div>
+					<div className="profileRightBottom">
+						<Feed profileid={urluserid} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
