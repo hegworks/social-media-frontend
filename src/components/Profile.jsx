12a5ac609d405b6e1540836eb0cf3f94915a91ab
Farbod Nazari <81690217+farbodnm@users.noreply.github.com>
2021-07-03 19:20:12 +0430

    Add username and description

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router";
-
+import axios from "axios"
 import { CookieManager } from "./CookieManager";
 import Topbar from "./Topbar";
 import SideBar from "./SideBar";
@@ -10,35 +10,45 @@ import AddPost from "./AddPost";
 import "./styles/profile.scss";
 
 const Profile = props => {
-	const urluserid = useParams().id;
-	console.log(urluserid);
+  const urluserid = useParams().id;
+  console.log(urluserid);
 
-	const { userid } = useContext(CookieManager);
-	console.log(userid);
+  const { userid } = useContext(CookieManager);
+  console.log(userid);
 
-	return (
-		<div className="page">
-			<Topbar />
-			<div className="profile">
-				<SideBar />
-				<div className="profileRight">
-					<div className="profileRightTop">
-						<div className="profileCover">
-							<img className="profileCoverImg" alt="" />
-						</div>
-						<div className="profileInfo">
-							<h4 className="profileInfoName">username goes here</h4>
-							<span className="profileInfoDesc">description</span>
-						</div>
-					</div>
-					<div>{userid === urluserid && <AddPost />}</div>
-					<div className="profileRightBottom">
-						<Feed profileid={urluserid} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`http://localhost:8880/api/users/${urluserid}`);
+      setUser(res.data);
+    }
+    fetchUser()
+  }, [urluserid])
+
+  return (
+    <div className="page">
+      <Topbar />
+      <div className="profile">
+        <SideBar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img className="profileCoverImg" alt="" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">{user.username}</h4>
+              <span className="profileInfoDesc">{user.desc}</span>
+            </div>
+          </div>
+          <div>{userid === urluserid && <AddPost />}</div>
+          <div className="profileRightBottom">
+            <Feed profileid={urluserid} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
